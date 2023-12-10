b77f863fc0a3ac0726fc93de306fdc1b0ee2c434
Jagannath Guptha Kasula <jagan@jagannaths-air.lan>
2023-12-10 10:19:37 -0600

    Dec10

--- app/ai.py
+++ app/ai.py
@@ -3,3 +3,6 @@ from transformers import pipeline
 def captioning(image):
     captioner = pipeline("image-to-text",model="Salesforce/blip-image-captioning-base")
     return captioner(image)
+
+if __name__ == '__main__':
+   print( captioning('nature4.jpeg'))
